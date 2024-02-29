44aca133762f874449e400497d006e9ffda0cd12
Mykhailo Moskalets <69348201+13MrBlackCat13@users.noreply.github.com>
2024-02-29 10:57:56 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,11 +1,32 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+// Create web server
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var path = require('path');
+var filePath = path.join(__dirname, 'comments.json');
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/comments', function (req, res) {
+  fs.readFile(filePath, function (err, data) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(data);
+  });
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.post('/comments', function (req, res) {
+  fs.readFile(filePath, function (err, data) {
+    var comments = JSON.parse(data);
+    comments.push(req.body);
+    fs.writeFile(filePath, JSON.stringify(comments, null, 2), function (err) {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(comments));
+    });
+  });
+});
+
+app.listen(3000, function () {
+  console.log('Server is running on port 3000');
 });
